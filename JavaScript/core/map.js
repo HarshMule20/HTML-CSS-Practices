9eccff79dcf41ca704d7f43c33df870edb5cb169
unknown <hmuley22@gmail.com>
2020-06-15 18:54:23 +0530

    Array filtration using the map method

---
+++ JavaScript/core/map.js
@@ -0,0 +1,10 @@
+const hobbies = ['Cricket', 'TableTennis', 'Coding', 'Surfing'];
+
+// Using the proper for loop to fetch all the values in the list
+// for (let hobby of hobbies){
+//     console.log("My hobbies are: ", hobby);
+// }
+
+// Using the map function of JavaScript =>
+
+console.log(hobbies.map(hobby => 'Hobby: '+ hobby));
