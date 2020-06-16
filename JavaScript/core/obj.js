796e3bb959822cbf9c808d3a285b0ccfc15130a8
unknown <hmuley22@gmail.com>
2020-06-16 18:46:06 +0530

    use of spread

--- JavaScript/core/obj.js
+++ JavaScript/core/obj.js
@@ -20,3 +20,18 @@ for (let hobby of hobbies){
 const newarr = [...hobbies];
 console.log("Actual Array:", hobbies);
 console.log("Copied array:", newarr);
+
+const newobj = {...obj};
+console.log(newobj);
+
+// using the rest operator
+// const arr = (arg1, arg2, arg3) => {
+//     return [arg1, arg2, arg3];
+// };
+// it cant accept more than three as shown!
+
+const arr = (...args) => {
+    return args;
+}
+// for that the rest operator is used
+console.log(arr(1, 2, 3, 4));
